65856803b4f8c4bd5c156fa37f47d33b307bd23d
NHQ9299 <quannhph08777@fpt.edu.vn>
2020-03-27 20:19:09 +0700

    2 files modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavComponent } from './nav/nav.component';
+import { SlideComponent } from './slide/slide.component';
+import { FooterComponent } from './footer/footer.component';
+import { DanhSachSanPhamComponent } from './danh-sach-san-pham/danh-sach-san-pham.component';
+import { QuanLySanPhamComponent } from './quan-ly-san-pham/quan-ly-san-pham.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  declarations: [ AppComponent, HelloComponent, NavComponent, SlideComponent, FooterComponent, DanhSachSanPhamComponent, QuanLySanPhamComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
