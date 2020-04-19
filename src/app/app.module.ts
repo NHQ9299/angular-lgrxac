1da81de9329179a97ffc8d8bd67f0296bbf14312
NHQ9299 <quannhph08777@fpt.edu.vn>
2020-04-19 08:38:39 +0700

    đssdfsdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavComponent } from './nav/nav.component';
 import { SlideComponent } from './slide/slide.component';
 import { FooterComponent } from './footer/footer.component';
-import { DanhSachSanPhamComponent } from './danh-sach-san-pham/danh-sach-san-pham.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductService } from './services/product.service';
-import { ChiTietComponent } from './chi-tiet/chi-tiet.component';
+
 import { QuanLySanPhamComponent } from '../backend/quan-ly-san-pham/quan-ly-san-pham.component';
 import { EditSanPhamComponent } from '../backend/edit-san-pham/edit-san-pham.component';
 import { AddSanPhamComponent } from '../backend/add-san-pham/add-san-pham.component';
-import { LoginComponent } from './login/login.component';
+import { DanhSachSanPhamComponent } from '../backend/danh-sach-san-pham/danh-sach-san-pham.component';
+import { ChiTietComponent } from '../backend/chi-tiet/chi-tiet.component';
+import { LoginComponent } from '../backend/login/login.component';
+
 
 
 @NgModule({
